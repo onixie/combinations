dbf87661cc86054ad34490b982982351399efbbc
O.Nixie <onixie@gmail.com>
2011-06-02 12:04:08 +0900

    Complete: 1. Modify with-combination and remove compile time evaluation of runtime symbols. 2. Modify the return value of with-combination, let it be the same with the last map-over-combinations. This is clearer and better. 3. Modify generation-combinations to use with-combinations, and enable it to specify multiple count 4. Modify sum-of-combinations to use with-combinations 5. Remove the export of functions other than combination function, those code may be later reexport after i inspected. 6. Export sum-of-combinations
    To Do:
    Add asdf support
    Rename a better file name and package name
    Add Regression test support
    Add sum subsets code from somewhere i got
    Inspect the rest of code

--- statistics.lisp
+++ statistics.lisp
@@ -2,48 +2,30 @@
 
 (cl:defpackage #:omnistat
   (:use #:cl)
-  (:export #:generate-combinations
-	   #:map-over-combinations
+  (:export #:map-over-combinations
+	   #:generate-combinations
 	   #:with-combinations
-	   #:random-permutation
-	   #:lotto
-	   #:random-select
-	   #:make-range-integers))
+	   #:sum-of-combinations))
 
 (cl:in-package #:omnistat)
 
-;;; МАТЕМАТИЧЕСКИЕ МЕТОДЫ В ИНФОРМАТИКЕ
-;;  Генерирование всех number-элементных подмножеств множества (1, ... ,n).
-;; (defun generate-combination (count list)
-;;   "Generate the combinations of K distinct objects chosen from the N elements of a list"
-;;   (cond ((> count (length list))
-;; 	 (error "длина генерируемого списка не может быть больше начального списка!"))
-;; 	((= 0 count) (list nil))	;  при нуле элементов в подмножестве - нет комбинаций.
-;; 	((eql count (length list)) (list list))
-;; 	(t
-;; 	 (let ((result nil) (combinations nil))
-;; 	   (dotimes (i count result) (push (1+ i) result)) ; первое подмножество
-;; 	   (setf result (reverse result))
-;; 	   (do ((number count))
-;; 	       ((< number 1) (reverse combinations))
-;; 	     (push 
-;; 	      (mapcar (lambda (item) (elt list (1- item))) (copy-list result))
-;; 	      combinations)
-;; 	     (if (eql (elt result (1- count)) (length list))
-;; 		 (setf number (1- number))
-;; 		 (setf number count))
-;; 	     (when (>= number 1) 
-;; 	       (do ((i count (1- i)))
-;; 		   ((< i number) nil) 
-;; 		 (setf (elt result (1- i)) (- (+ (elt result (1- number)) i 1) number)))))))))
-(defvar *verbose* t)
+(defvar *rattle-on* t
+  "Enable printing while calculating")
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun map-over-combinations (count list &optional (mapfunc #'(lambda (combination) (format *verbose* "~&~s " combination) nil)))
-    "Map function over the combinations of K distinct objects chosen from the N elements of a list"
-    (format *verbose* "~&Specified count ~s with list ~s~%" count list)
+  (defun ensure-list (object)
+    "Ensure object is a list (if it's an atom, put it in a list)"
+    (cond ((and (not (null object)) (atom object)) (list object))
+	  (t object))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun map-over-combinations (count list &optional (mapfunc #'(lambda (combination) 
+								  (format *rattle-on* "~&~s " combination) 
+								  nil)))
+    "Map function over the combinations of K distinct objects chosen from a list of N elements" 
+    (format *rattle-on* "~&Select combinations of ~s elements in list ~s~%" count list)
     (cond ((> count (length list))
-	   (error "The count specified is bigger than the list size"))
+	   (error "The specified number of elements for combination is bigger than the list size"))
 	  ((= 0 count) 
 	   (funcall mapfunc nil))	;  при нуле элементов в подмножестве - нет комбинаций.
 	  ((eql count (length list)) 
@@ -53,8 +35,8 @@
 	     (dotimes (i count result) (push (1+ i) result)) ; первое подмножество
 	     (setf result (reverse result))
 	     (do ((number count))
-		 ((< number 1) mapfunc-result)
-	       (setq mapfunc-result (funcall mapfunc
+		 ((< number 1) mapfunc-result) ;Return value is the last mapfunc's call
+	       (setf mapfunc-result (funcall mapfunc
 					     (mapcar (lambda (item) 
 						       (elt list (1- item)))
 						     (copy-list result))))
@@ -66,51 +48,43 @@
 		     ((< i number) nil) 
 		   (setf (elt result (1- i)) (- (+ (elt result (1- number)) i 1) number))))))))))
 
-;;Because i eval the args in macro expansion time, it may have problem when invoked with runtime objects as args.
-;; Is there a way to prevent macro expansion until execution time??
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defmacro with-combinations ((combination count-or-counts list) &body body)
-    ""
-    (let ((list `(list ,@(eval list)))
-	  (count-or-counts (eval count-or-counts)))
-      (cond ((null count-or-counts) nil)
-	    ((and (atom count-or-counts) (integerp count-or-counts))
-	     `(map-over-combinations ,count-or-counts ,list (lambda (,combination)
-							      ,@body)))
-	    ((consp count-or-counts)
-	     `(append (with-combinations (,combination (car (list ,@count-or-counts)) ,list)
-			,@body)
-		      (with-combinations (,combination (cdr (list ,@count-or-counts)) ,list)
-			,@body)))
-	    (t nil)))))
+(defmacro with-combinations ((combination count-or-counts list) &body body)
+  "Call body form in a bunch of combinations with a list of specified number of elements"
+  (let ((ressym (gensym))
+	(countsym (gensym)))
+    `(let ((,ressym nil))  ;for eliminate compilation warning in sbcl
+       (dolist (,countsym (ensure-list ,count-or-counts) ,ressym)  ;keep the action of return value as map-over-combinations, the last call of map-over-combinations
+	 (setq ,ressym
+	       (map-over-combinations ,countsym ,list (lambda (,combination)
+							,@body))))
+       ,ressym)))
 
-(defun generate-combinations (count list)
-  "Create a list of combinations"
+(defun generate-combinations (count-or-counts list)
+  "Create a list of combinations with specified number of elements"
   (let ((combinations nil))
-    (map-over-combinations count list #'(lambda (combination)
-					  (push combination combinations)))))
+    (with-combinations (combination count-or-counts list)
+      (push combination combinations))
+    (nreverse combinations)))
 
-(defun sum-of-combinations (count-or-counts list &key (expected-sums nil) (collect-criteria #'(lambda (sum-of-combination expected-sums-with-combinations) t)))
-  "Calculate sum subset for subsets with specified number of elements"
+(defun sum-of-combinations (count-or-counts list &key (expected-sums nil) (collect-criteria #'(lambda (sum-of-combination expected-sums-with-combinations) 
+												(declare (ignorable sum-of-combination expected-sums-with-combinations))
+												t)))
+  "Calculate sum of subset for subsets with specified number of elements"
   (let* ((list list)
 	 (sum-of-list (reduce #'+ list))
-	 (expected-sums (cond ((atom expected-sums) (list expected-sums))
-			      (t expected-sums)))
+	 (expected-sums (ensure-list expected-sums))
 	 (sums-of-combinations nil)
 	 (expected-sums-with-combinations nil))
-     (format *verbose* "List are ~S~%Sum of list are ~d~%" list sum-of-list)
-     (mapcar #'(lambda (count) 
-		 (map-over-combinations count list
-					#'(lambda (combination)
-					    (let* ((sum-of-combination (round (reduce #'+ combination))))
-					      (pushnew sum-of-combination sums-of-combinations)
-					      (cond ((member sum-of-combination expected-sums)
-						     (if (funcall collect-criteria sum-of-combination expected-sums-with-combinations)
-							 (pushnew (cons sum-of-combination (list combination)) expected-sums-with-combinations)))
-						    (t nil))))))
-	     (cond ((atom count-or-counts) (list expected-sums))
-		   (t count-or-counts)))
+     (format *rattle-on* "List are ~S~%Sum of list are ~d~%" list sum-of-list)
+     (with-combinations (combination count-or-counts list)
+       (let* ((sum-of-combination (round (reduce #'+ combination))))
+	 (pushnew sum-of-combination sums-of-combinations)
+	 (cond ((member sum-of-combination expected-sums)
+		(if (funcall collect-criteria sum-of-combination expected-sums-with-combinations) ;when nil, do not collect this sum of combination
+		    (pushnew (cons sum-of-combination (list combination)) expected-sums-with-combinations)))
+	       (t nil))))
      (values (sort expected-sums-with-combinations #'< :key #'car) (sort sums-of-combinations #'<))))
+
 
 ;; Число сочетаний из n элементов по m
 ;; (defun cmn (m n)
@@ -163,76 +137,3 @@
   "Draw N different random numbers from the set 1..M."
   (random-select		; Создать список из списка.
    (make-range-integers 1 range) count))	; Создать список содержащий все целые числа в заданном диапазоне
-
-;; This one will cause heap exhausted, we'd better find a functional way to handle it.
-;; (defun reverse-back (&key (weight-count 30) (max-weight 25) (default-select-counts (list 1 2 3 4)))
-;;   (let* ((weights (lotto weight-count max-weight))
-;; 	 (select-counts default-select-counts)
-;; 	 (weight-sum (apply '+ weights))
-;; 	 (select-sum-set (sort (remove-duplicates (mapcar #'(lambda (select)
-;; 							(round (* (/ (apply '+ select) weight-sum) 100)))
-;; 						    (mapcan #'(lambda (count)
-;; 								(generate-combination count weights)
-;; 								)
-;; 							    select-counts))) #'>)))
-;;     (values weights weight-sum select-sum-set)))
-
-;; (defmacro omnifactor (&key (weight-count 30) (max-weight 25) (default-value-of-expected '(1 2 3)) (default-count-selects '(1 2 3)))
-;;   `(let* ((weights (omnistat:lotto ,weight-count ,max-weight))
-;; 	  (sum-of-weights (reduce #'+ weights))
-;; 	  (value-of-expected (list ,@default-value-of-expected))
-;; 	  (all-of-result nil)
-;; 	  (expected-result nil))
-;;      (format t "Weights are ~S~&Sum of weights are ~d~&" weights sum-of-weights)
-;;      (omnistat::with-combinations (combination ,default-count-selects weights)
-;;        (let* ((sum-of-combination (reduce #'+ combination))
-;; 	      (value-of-result (round (* (/ sum-of-combination sum-of-weights) 100))))
-;; ;	 (format t "Combination are ~S~&Sum of combination are ~d~&" combination sum-of-combination)
-;; ;	 (format t "Value (sum-of-combination/sum-of-weight*100) are ~d~&" value-of-result)
-;; 	 (pushnew value-of-result all-of-result)
-;; 	 (cond ((member value-of-result value-of-expected)
-;; 		(prog1 (pushnew (cons value-of-result (list combination)) expected-result)
-;; 		  (if (>= (count value-of-result expected-result :key #'car) 2)
-		      
-;; 		      (print expected-result))))
-;; 	       (t nil))))
-;;      (values sum-of-weights (sort all-of-result #'<) (sort expected-result #'< :key #'car))))
-
-;; (defmacro omnifactorv2 (&key (default-weights '(1 2 3)) (default-value-of-expected '(1 2 3)) (default-count-selects '(1 2 3)) (default-result-count 5))
-;;   `(let* ((weights (list ,@default-weights))
-;; 	  (sum-of-weights (reduce #'+ weights))
-;; 	  (value-of-expected (list ,@default-value-of-expected))
-;; 	  (all-of-result nil)
-;; 	  (expected-result nil)
-;; 	  (result-count ,default-result-count))
-;;      (format t "Weights are ~S~&Sum of weights are ~d~&" weights sum-of-weights)
-;;      (omnistat::with-combinations (combination ,default-count-selects weights)
-;;        (let* ((sum-of-combination (reduce #'+ combination))
-;; 	      (value-of-result (round sum-of-combination)))
-;; ;	 (format t "Combination are ~S~&Sum of combination are ~d~&" combination sum-of-combination)
-;; ;	 (format t "Value (sum-of-combination/sum-of-weight*100) are ~d~&" value-of-result)
-;; 	 (pushnew value-of-result all-of-result)
-;; 	 (cond ((member value-of-result value-of-expected)
-;; 		(progn 
-;; 		  (if (< (count value-of-result expected-result :key #'car) result-count)
-;; 		      (pushnew (cons value-of-result (list combination)) expected-result)
-;; 					;(print expected-result)
-;; 		      )
-;; 		  expected-result))
-;; 	       (t nil))))
-;;      (values sum-of-weights (sort all-of-result #'<) (sort expected-result #'< :key #'car))))
-
-
-;; (omnistat::omnifactorv2 :default-weights (6.94 1.98 2.54 2.33 5.38 1.72 2.04 3.76 6.09 6.19 3.9 0.57 9.65 2.45 6.78 4.55 6.99 4.16 3.58 1.36 2.73 1.19 1.47 2.15 2.72 4.51 1.86 0.4 1.86 0.4)
-;; 				 :default-value-of-expected (48 73 79 54 64 60 29 70 53 42 64 59 40 49 57 63 44 61 72 62 62 47 42 46 52 37 54 57 69 37 46 59 38 56 53 33 53 58 41 56 63 59 60 32 72 41 46 73 61 51 53 43 47 55 34 46 76 66 25 73 47 50 63 45 52 50 50 57 46 63 63 57 33 60 38 43 35 48 47 42 53 52 60 44 40 45 54 51 51 59 50 74 70 45 24 43 75 68 36 32 16 34 39 39 49 57 46 72 33 59 60 48 68 68 66 49 57 56 48 57 47)
-;; 				 :default-count-selects (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
-;; 				 )
-
-
-;; (sort (remove-duplicates (list 48 73 79 54 64 60 29 70 53 42 64 59 40 49 57 63 44 61 72 62 62 47 42 46 52 37 54 57 69 37 46 59 38 56 53 33 53 58 41 56 63 59 60 32 72 41 46 73 61 51 53 43 47 55 34 46 76 66 25 73 47 50 63 45 52 50 50 57 46 63 63 57 33 60 38 43 35 48 47 42 53 52 60 44 40 45 54 51 51 59 50 74 70 45 24 43 75 68 36 32 16 34 39 39 49 57 46 72 33 59 60 48 68 68 66 49 57 56 48 57 47)) #'<)
-
-;; (sort (list 48 73 79 54 64 60 29 70 53 42 64 59 40 49 57 63 44 61 72 62 62 47 42 46 52 37 54 57 69 37 46 59 38 56 53 33 53 58 41 56 63 59 60 32 72 41 46 73 61 51 53 43 47 55 34 46 76 66 25 73 47 50 63 45 52 50 50 57 46 63 63 57 33 60 38 43 35 48 47 42 53 52 60 44 40 45 54 51 51 59 50 74 70 45 24 43 75 68 36 32 16 34 39 39 49 57 46 72 33 59 60 48 68 68 66 49 57 56 48 57 47) #'<)
-
-;; (omnistat::omnifactorv2 :default-weights (6.94 1.98 2.54 2.33 5.38 1.72 2.04 3.76 6.09 6.19 3.9 0.57 9.65 2.45 6.78 4.55 6.99 4.16 3.58 1.36 2.73 1.19 1.47 2.15 2.72 4.51 1.86 0.4 1.86 0.4)
-;; 				 :default-value-of-expected (16 24 25 29 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 66 68 69 70 72 73 74 75 76 79)
-;; 				 :default-count-selects (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15))
